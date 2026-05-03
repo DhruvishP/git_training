b5b256cdbe3f45824b1e203a231f2f0017853399
DhruvishP <dhruvishparmar@hotmail.com>
2026-05-03 17:42:37 +0200

    added subtract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, subtract_nums
 
 if __name__ == "__main__":
     result = add_nums(5, 10)
     print(f"The result of adding 5 and 10 is: {result}")
+    print(f"The result of subtracting 10 from 5 is: {subtract_nums(5, 10)}")
