080b90a4f8c1eea4238510391fd6d698b870dcb7
DhruvishP <dhruvishparmar@hotmail.com>
2026-05-03 17:54:31 +0200

    added multiply and updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from utils.calculator import add_nums, divide_nums,subtract_nums, power_nums
+from utils.calculator import add_nums, divide_nums,subtract_nums, power_nums,multiply_nums
 
 if __name__ == "__main__":
     print(add_nums(5, 10))
     print(divide_nums(10, 2))
     print(subtract_nums(10, 5))
     print(power_nums(2, 3))
+    print(multiply_nums(5, 10))
 
--- utils/calculator.py
+++ utils/calculator.py
@@ -8,3 +8,5 @@ def subtract_nums(a, b):
 
 def power_nums(a, b):
     return a ** b
+def multiply_nums(a, b):
+    return a * b
