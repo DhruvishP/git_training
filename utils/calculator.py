96640661783600754564d835683852f05fffcb94
DhruvishP <dhruvishparmar@hotmail.com>
2026-05-04 05:58:30 +0200

    formatted calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -3,10 +3,12 @@ def add_nums(a, b):
 
 def divide_nums(a, b):
     return a / b
+
 def subtract_nums(a, b):
     return a - b
 
 def power_nums(a, b):
     return a ** b
+
 def multiply_nums(a, b):
     return a * b
